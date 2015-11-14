3be0f446aa2162a2f6e4557ccbe4743f97c28592
Ubuntu <ubuntu@ip-172-31-27-95.ap-northeast-1.compute.internal>
2015-11-14 11:43:04 +0000

    server add

--- System_Programming/tcp_server.js
+++ System_Programming/tcp_server.js
@@ -1,15 +1,33 @@
 var net = require('net');
-
+var port = 1337;
 var server = net.createServer(function(socket) {
-    socket.write('Echo server\r\n');
-    socket.pipe(socket);
+	console.log("new Connction");
+	socket.setEncoding("utf8");
+
+	// send(socket, "Echo Server");
+
+	socket.on('data',function(data){
+		console.log(data.toString());
+		socket.write(data);
+	});
+
+	socket.on("connection", function(){
+		console.log("------connection------");
+		rePrompt(socket);
+	});
+
+
+	socket.on("end", function(){
+		console.log("Client Leave!!");
+	});
+
+	socket.on("error",function(err){
+		console.log(err);
+	});
+
 });
 
-server.on('data', function(data){
-    console.log("server received : " +data);
-}
 
-);
+server.listen(port);
 
-
-server.listen(1337, '127.0.0.1');
+console.log("server start in 52.69.176.156 port 1337");
