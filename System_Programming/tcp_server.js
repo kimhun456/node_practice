837a2079706e52b693cde178ee675aaf8562b6de
aws_hyunjae <kimhun456@gmail.com>
2015-11-15 08:14:58 +0000

    chat server edited

--- System_Programming/tcp_server.js
+++ System_Programming/tcp_server.js
@@ -1,11 +1,12 @@
 var net = require('net');
 var port = 1337;
+var clients = [];
 var server = net.createServer(function(socket) {
-	console.log("--------connection------");
 	socket.setEncoding("utf8");
-
+	socket.name = socket.remoteAddress + ":" + socket.remotePort ;
 	// send(socket, "Echo Server");
-
+	clients.push(socket);
+	console.log("----------"+socket.name + " is connected------");
 	socket.on('data',function(data){
 		console.log("recieved data : " + data.toString());
 		socket.write(data);
@@ -14,6 +15,7 @@ var server = net.createServer(function(socket) {
 
 	socket.on("end", function(){
 		console.log("--------client leave --------");
+		clients.splice(clients.indexOf(socket),1);
 	});
 
 	socket.on("error",function(err){
@@ -26,3 +28,11 @@ var server = net.createServer(function(socket) {
 server.listen(port);
 
 console.log("server start in 52.69.176.156 port 1337");
+
+function broadcast(message, sender){
+	clinets.forEach(function (client){
+		client.write(message);
+	});
+}
+
+//https://gist.github.com/creationix/707146
