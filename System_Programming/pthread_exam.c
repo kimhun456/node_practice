acba99da041b563e5cf44ebc2c60c7ee9414e8b8
김현재 <kimhun456@gmail.com>
2015-11-16 13:10:53 +0900

    sort the folder

--- System_Programming/pthread_exam.c
+++ System_Programming/pthread_exam.c
@@ -1,39 +1,38 @@
-
+#include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <signal.h>
-#include <pthread.h> // gcc  test.c -o test -lpthread
+#include <stdlib.h>
 
-
-void* My(void* Para)
+// 쓰레드
+// 1초를 기다린후 아규먼트^2 을 리턴한다.
+void *t_function(void *data)
 {
- int i;
-  for(i=0;i<10;i++)
-  {
-   printf("[+] Child : Hello World ! \n");
-   sleep(2);
-  }
+  printf("%s\n",(char*)data);
+	printf("Thread Start\n");
+    sleep(1);
+	printf("Thread end\n");
 }
-
 
 int main()
 {
- pthread_t re;
- int a;
- void* s;
+    pthread_t p_thread;
+    int thr_id;
+    int status;
+    int a = 100;
+    char input[1000];
+    scanf("%s",input);
+    printf("%s\n",input);
+    printf("Before Thread\n");
+    thr_id = pthread_create(&p_thread, NULL, t_function, (void *)input);
+    if (thr_id < 0)
+    {
+        perror("thread create error : ");
+        exit(0);
+    }
 
- a = pthread_create(&re,NULL,My,NULL);
-
-
- while(1)
- {
-   printf("[+] Parent : Hello World !\n");
-   sleep(1);
- }
- pthread_join(re,&s);
- printf("%s\n",(char*)s);
-
- return 0;
+    // 식별번호 p_thread 를 가지는 쓰레드를 detach
+    // 시켜준다.
+    pthread_detach(p_thread);
+    pause();
+    return 0;
 }
