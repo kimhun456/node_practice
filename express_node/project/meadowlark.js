4d6603af4f2f277a7b5335210924e62fa51e7ce1
swm_HyunJae <kimhun456@gmail.com>
2015-11-26 22:57:45 +0900

    chap05

--- express_node/project/meadowlark.js
+++ express_node/project/meadowlark.js
@@ -18,14 +18,20 @@ console.log("static folder is added");
 // 이 방법 사용시 서버를 시작하기전에 환경 값을 설정해 포트를 오버라이드 가능하다.
 app.set("port",process.env.PORT||3000);
 
-
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+	next();
+});
 // get()은 라우터를 추가하는 메서드이다
 app.get("/", function(req,res){
     res.render("home");
 });
 
 app.get("/about", function(req,res){
-  res.render("about", {fortunes: fortune.getFortunes() })
+  res.render("about", {
+    fortunes: fortune.getFortunes(),
+    pageTestScript : "/qa/test-about.js" })
 });
 
 // 404 커스텀 페이지
